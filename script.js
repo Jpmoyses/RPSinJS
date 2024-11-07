8ce4c8d35f6eb91f5eea5bc7cb1617ff2d5b115e
jpmoyses <jpmoyses00@gmail.com>
2024-11-07 16:11:39 -0300

    Finished UI and JS code

--- script.js
+++ script.js
@@ -1,71 +1,110 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanScoreUI = document.getElementById("humanScore");
+const computerScoreUI = document.getElementById("computerScore");
+const result = document.getElementById("result");
+const botButton = document.querySelectorAll(".disable");
+
 function getComputerChoice(){
     let i = Math.floor(Math.random() * 100) + 1;
 
-    if(i < 33){return "paper";}
-    else if (i > 66){return "rock";}
-    else {return "scissors";}
+    if(i < 33){
+        addBGBot("paper");
+        return "paper";
+    }
+    else if (i > 66){
+        addBGBot("rock");
+        return "rock";
+    }
+    else {
+        addBGBot("scissors");
+        return "scissors";
+    }
 }
 
-function getHumanChoice(){
-    let answer = null;
-    /*fixes cancel bug on the prompt*/
-    do{
-        answer = prompt("Choose between rock, paper and scissors:");
-    }while(answer == null);
+function addBGBot(id){
+    botButton.forEach((item) => {
+        if (item.id == id){
+            item.classList.add("bgClick");
+        }
+    })
+}
 
-    answer = answer.toLowerCase();
-    answer = answer.trim();
-    if ((answer === "rock" || answer === "paper" || answer === "scissors") == true){
-        return answer;
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win, " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+        humanScoreUI.innerText = humanScore;
+        result.classList.add("win");
+        result.classList.remove("lost");
+        result.innerText = "You win!";
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win, " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+        humanScoreUI.innerText = humanScore;
+        result.classList.add("win");
+        result.classList.remove("lost");
+        result.innerText = "You win!";
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win, " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+        humanScoreUI.innerText = humanScore;
+        result.classList.add("win");
+        result.classList.remove("lost");
+        result.innerText = "You win!";
+    }
+    else if(humanChoice === computerChoice){
+        console.log("Draw!")
+        result.classList.remove("win");
+        result.classList.remove("lost");
+        result.innerText = "Draw!";
     }
     else{
-        alert("Error, try again.");
-        return getHumanChoice();
+        console.log("You lose, " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+        computerScoreUI.innerText = computerScore;
+        result.classList.remove("win");
+        result.classList.add("lost");
+        result.innerText = "You lost!";
     }
+
+    if(humanScore == 5 || computerScore == 5) endGame();
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    let x = 1;
+// add event listener to buttons
+const btn = document.querySelectorAll("button");
+btn.forEach( (button) => {
+    button.addEventListener("click", () =>{
+        removeBG();
+        playRound(button.id, getComputerChoice());
+        button.classList.add("bgClick");
+        if(humanScore == 0 && computerScore == 0) endGame();
+    })
+})
 
-    function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()){
-        console.log("Round " + x);
-        if (humanChoice === "paper" && computerChoice === "rock"){
-            console.log("You win, " + humanChoice + " beats " + computerChoice);
-            humanScore++;
-        }
-        else if(humanChoice === "scissors" && computerChoice === "paper"){
-            console.log("You win, " + humanChoice + " beats " + computerChoice);
-            humanScore++;
-        }
-        else if(humanChoice === "rock" && computerChoice === "scissors"){
-            console.log("You win, " + humanChoice + " beats " + computerChoice);
-            humanScore++;
-        }
-        else if(humanChoice === computerChoice){
-            console.log("Draw!")
-        }
-        else{
-            console.log("You lose, " + computerChoice + " beats " + humanChoice);
-            computerScore++;
-        }
-        x++;
-    }
-
-    for(let i = 0; i < 5; i++){
-        playRound();
-    }
-
-    if(humanScore > computerScore){
-        console.log("You win! " + humanScore + " x " + computerScore);
-    }
-    else if(humanScore == computerScore){
-        console.log("Its a draw!" + humanScore + " x " + computerScore);
-    }
-    else{
-        console.log("You lose! " + humanScore + " x " + computerScore);
-    }
+function removeBG(){
+    btn.forEach((button) =>{
+        button.classList.remove("bgClick");
+    })
 }
 
-while(true) { playGame(); }
+function endGame(){
+    if(humanScore == 5){
+        alert("You win!");
+    }
+    if(computerScore == 5){
+        alert("You lose!");
+    }
+
+    removeBG();
+    humanScore = 0;
+    humanScoreUI.innerText = humanScore;
+    computerScore = 0;
+    computerScoreUI.innerText = computerScore;
+    result.classList.remove("win");
+    result.classList.remove("lost");
+    result.innerText = "Click one!";
+}
